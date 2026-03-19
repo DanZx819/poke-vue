d739408d2030f28a967d087fd765fa2e0393a09c
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-19 20:20:34 -0300

    Transformando em PWA

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
@@ -14,6 +15,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
